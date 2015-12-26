38367e8ad7f05605ba7179c3be84f95e6154eb73
Masami Hiramatsu <masami.hiramatsu@gmail.com>
2015-12-26 21:41:46 +0900

    build_kernel: Support cgroups

--- src/2_build_kernel.sh
+++ src/2_build_kernel.sh
@@ -11,6 +11,14 @@ make mrproper
 # Create a default configuration file for the kernel
 make defconfig
 
+yconfig() { # configs
+while [ $# -ne 0 ]; do
+  sed -i "s/.*CONFIG_$1\ .*/CONFIG_$1=y/" .config
+  grep ^"CONFIG_$1=y" .config || echo "CONFIG_$1=y" >> .config
+  shift 1
+done
+}
+
 # Changes the name of the system
 sed -i "s/.*CONFIG_DEFAULT_HOSTNAME.*/CONFIG_DEFAULT_HOSTNAME=\"boot2minc\"/" .config
 
@@ -29,6 +37,12 @@ echo "CONFIG_8139CP=y" >> .config
 # Config for MINC support
 sed -i "s/.*CONFIG_OVERLAY_FS\ .*/CONFIG_OVERLAY_FS=y/" .config
 sed -i "s/.*CONFIG_SQUASHFS\ .*/CONFIG_SQUASHFS=y/" .config
+
+# Config for cgroups
+yconfig CGROUPS EVENTFD CGROUP_DEVICE CPUSETS CGROUP_CPUACCT \
+        PAGE_COUNTER MEMCG MEMCG_SWAP MEMCG_SWAP_ENABLED \
+        CGROUP_PERF CGROUP_SCHED CGROUP_HUGETLB FAIR_GROUP_SCHED \
+        CFS_BANDWIDTH RT_GROUP_SCHED BLK_CGROUP
 
 make olddefconfig
 
